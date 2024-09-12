7a5d1e1df88dd30cb37a9fa3a918d5724deb9222
kauatwn <kaualopescontato@gmail.com>
2024-09-11 21:32:50 -0300

    Criando abstração IBookMapper

---
+++ src/Application/Abstractions/Mappings/IBookMapper.cs
@@ -0,0 +1,9 @@
+﻿using Application.DTOs.Responses.Book;
+using Domain.Entities;
+
+namespace Application.Abstractions.Mappings;
+
+public interface IBookMapper
+{
+    GetBookByIdResponse DomainToResponse(Book book);
+}
