f0175e1ac0e371e73d7b3796236dab9671d8ef09
kauatwn <kaualopescontato@gmail.com>
2024-09-11 21:39:10 -0300

    Criando teste unitário para GetBookByIdUseCase

---
+++ test/Application.UnitTests/UseCases/Book/GetBookByIdUseCaseTest.cs
@@ -0,0 +1,55 @@
+﻿using Application.Abstractions.Mappings;
+using Application.DTOs.Responses.Book;
+using Application.UseCases.Book;
+using Domain.Interfaces.Repositories.Book;
+using Moq;
+
+namespace Application.UnitTests.UseCases.Book;
+
+public class GetBookByIdUseCaseTest
+{
+    private Mock<IBookMapper> MapperMock { get; } = new();
+    private Mock<IBookReadOnlyRepository> ReadOnlyRepositoryMock { get; } = new();
+    private GetBookByIdUseCase UseCase { get; }
+
+    public GetBookByIdUseCaseTest()
+    {
+        UseCase = new GetBookByIdUseCase(MapperMock.Object, ReadOnlyRepositoryMock.Object);
+    }
+    
+    [Fact]
+    public async Task ShouldReturnGetBookByIdResponseWhenBookExists()
+    {
+        // Arrange
+        var book = new Domain.Entities.Book(1, "Title","Author");
+        
+        ReadOnlyRepositoryMock.Setup(r => r.GetBookByIdAsync(It.IsAny<int>()))
+            .ReturnsAsync(book);
+        
+        MapperMock.Setup(m => m.DomainToResponse(It.IsAny<Domain.Entities.Book>()))
+            .Returns(new GetBookByIdResponse(book.Id, book.Title, book.Author));
+        
+        // Act
+        var response = await UseCase.ExecuteAsync(book.Id);
+        
+        // Assert
+        Assert.NotNull(response);
+        Assert.Equal(book.Id, response.Id);
+        Assert.Equal(book.Title, response.Title);
+        Assert.Equal(book.Author, response.Author);
+    }
+    
+    [Fact]
+    public async Task ShouldThrowExceptionWhenBookNotFound()
+    {
+        // Arrange
+        ReadOnlyRepositoryMock.Setup(r => r.GetBookByIdAsync(It.IsAny<int>()))
+            .ThrowsAsync(new Exception("Book not found."));
+
+        // Act
+        var exception = await Assert.ThrowsAsync<Exception>(() => UseCase.ExecuteAsync(1));
+
+        // Assert
+        Assert.Equal("Book not found.", exception.Message);
+    }
+}
