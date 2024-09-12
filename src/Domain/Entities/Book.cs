323e9e1bb2696255bd7e55067f878e5926b009c9
kauatwn <kaualopescontato@gmail.com>
2024-09-11 21:26:52 -0300

    Criando entidade Book

---
+++ src/Domain/Entities/Book.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Entities;
+
+public class Book(int id, string title, string author)
+{
+    public int Id { get; private set; } = id;
+    public string Title { get; private set; } = title;
+    public string Author { get; private set; } = author;
+}
