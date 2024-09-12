c293bef84cd16fcbf9b72be279dae1df19cf1591
kauatwn <kaualopescontato@gmail.com>
2024-09-11 21:35:50 -0300

    Criando classe concreta GetBookByIdUseCase

---
+++ src/Application/UseCases/Book/GetBookByIdUseCase.cs
@@ -0,0 +1,27 @@
+﻿using Application.Abstractions.Mappings;
+using Application.Abstractions.UseCases.Book;
+using Application.DTOs.Responses.Book;
+using Domain.Interfaces.Repositories.Book;
+
+namespace Application.UseCases.Book;
+
+public class GetBookByIdUseCase(IBookMapper mapper, IBookReadOnlyRepository readOnlyRepository)
+    : IGetBookByIdUseCase
+{
+    private IBookMapper Mapper { get; } = mapper;
+    private IBookReadOnlyRepository ReadOnlyRepository { get; } = readOnlyRepository;
+
+    public async Task<GetBookByIdResponse> ExecuteAsync(int id)
+    {
+        var book = await ReadOnlyRepository.GetBookByIdAsync(id);
+
+        if (book is null)
+        {
+            throw new Exception("Book not found.");
+        }
+
+        var bookDto = Mapper.DomainToResponse(book);
+
+        return bookDto;
+    }
+}
