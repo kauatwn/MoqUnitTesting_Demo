76ee040ed0c0ca6bc627b024fadef22ef5823eeb
kauatwn <kaualopescontato@gmail.com>
2024-09-11 21:33:27 -0300

    Criando classe concreta BookMapper

---
+++ src/Application/Mappings/BookMapper.cs
@@ -0,0 +1,13 @@
+﻿using Application.Abstractions.Mappings;
+using Application.DTOs.Responses.Book;
+using Domain.Entities;
+
+namespace Application.Mappings;
+
+public class BookMapper : IBookMapper
+{
+    public GetBookByIdResponse DomainToResponse(Book book)
+    {
+        return new GetBookByIdResponse(book.Id, book.Title, book.Author);
+    }
+}
