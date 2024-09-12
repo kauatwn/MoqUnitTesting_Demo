e50ffe6d11414c2369c0487d9729dc2d8038a2f7
kauatwn <kaualopescontato@gmail.com>
2024-09-11 21:38:05 -0300

    Criando BookRepository

---
+++ src/Infrastructure/Repositories/BookRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using Domain.Interfaces.Repositories.Book;
+using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class BookRepository(AppDbContext dbContext) : IBookReadOnlyRepository
+{
+    private AppDbContext DbContext { get; } = dbContext;
+
+    public async Task<Book?> GetBookByIdAsync(int id)
+    {
+        return await DbContext.Books.AsNoTracking().FirstOrDefaultAsync(book => book.Id == id);
+    }
+}
