162c7bd135db722cfdc90780abf24a19c2049d0b
kauatwn <kaualopescontato@gmail.com>
2024-09-12 19:00:53 -0300

    Ajustando GetBookByIdUseCase para usar DTO de request

--- src/Application/UseCases/Book/GetBookByIdUseCase.cs
+++ src/Application/UseCases/Book/GetBookByIdUseCase.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Abstractions.Mappings;
 using Application.Abstractions.UseCases.Book;
+using Application.DTOs.Requests.Book;
 using Application.DTOs.Responses.Book;
 using Domain.Interfaces.Repositories.Book;
 
@@ -11,17 +12,17 @@ public class GetBookByIdUseCase(IBookMapper mapper, IBookReadOnlyRepository read
     private IBookMapper Mapper { get; } = mapper;
     private IBookReadOnlyRepository ReadOnlyRepository { get; } = readOnlyRepository;
 
-    public async Task<GetBookByIdResponse> ExecuteAsync(int id)
+    public async Task<GetBookByIdResponse> ExecuteAsync(GetBookByIdRequest request)
     {
-        var book = await ReadOnlyRepository.GetBookByIdAsync(id);
+        var book = await ReadOnlyRepository.GetBookByIdAsync(request.Id);
 
         if (book is null)
         {
             throw new Exception("Book not found.");
         }
 
-        var bookDto = Mapper.DomainToResponse(book);
+        var response = Mapper.DomainToResponse(book);
 
-        return bookDto;
+        return response;
     }
 }
